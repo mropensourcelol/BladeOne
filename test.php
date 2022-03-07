51975bef4d9d335ab2679211a66b4d5bb889cc20
mredchristie <mredchristie@yahoo.com>
2022-03-07 19:55:56 +0000

    template update

--- test.php
+++ test.php
@@ -3,8 +3,8 @@
 include '../BladeOne/lib/BladeOne.php';
 use eftec\bladeone\BladeOne;
 
-$views = __DIR__ . '/../views';
-$cache = __DIR__ . '/../cache';
+$views = __DIR__ . '/views';
+$cache = __DIR__ . '/cache';
 $blade = new BladeOne($views,$cache,BladeOne::MODE_DEBUG);
 echo $blade->run("index");
 
