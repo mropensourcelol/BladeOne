0ec006f21d2266b865ebc8cb6d0999e6b60a0485
OPENSOURCE <sourcemropen@gmail.com>
2022-03-15 10:07:39 +0000

    just testing stuff

--- views/app.blade.php
+++ views/app.blade.php
@@ -1,16 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<title>Bootstrap Example</title>
+<!DOCTYPE html5>
+<html>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" href="css/site.css">
-</head>
-<body>
-
-<nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+    <head>
+        <title>App Name - @yield('title')</title>
+    </head>
+    <body>
+        @section('sidebar')
+        <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
   <div class="container-fluid">
   <a class="navbar-brand" href="#">
       <img src="gibjohn.png" alt="Avatar Logo" style="width:40px;" class="rounded-pill"> 
@@ -34,111 +34,10 @@
     </div>
   </div>
 </nav>
+        @show
 
 
-
-
-
-<section class="vh-100 gradient-custom">
-  <div class="container py-5 h-100">
-    <div class="row justify-content-center align-items-center h-100">
-      <div class="col-12 col-lg-9 col-xl-7">
-        <div class="card shadow-2-strong card-registration" style="border-radius: 15px;">
-          <div class="card-body p-4 p-md-5">
-            <h3 class="mb-4 pb-2 pb-md-0 mb-md-5">Registration Form - Learner</h3>
-            <form>
-              <div class="row">
-                <div class="col-md-12 mb-8">
-                  <div class="form-outline">
-                    <input type="text" id="firstName" class="form-control form-control-lg" Placeholder="Full Name"/>
-                    <label class="form-label" for="firstName" >Full Name</label>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 mb-4 d-flex align-items-center">
-                  <div class="form-outline datepicker w-100">
-                    <input
-                      type="date"
-                      class="form-control form-control-lg"
-                      id="birthdayDate"
-                    />
-                    <label for="birthdayDate" class="form-label">Date of Birth</label>
-                  </div>
-                </div>
-                <div class="col-md-6 mb-4">
-                  <h6 class="mb-2 pb-1">Gender: </h6>
-                  <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="radio"
-                      name="inlineRadioOptions"
-                      id="femaleGender"
-                      value="option1"
-                      checked
-                    />
-                    <label class="form-check-label" for="femaleGender">Female</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="radio"
-                      name="inlineRadioOptions"
-                      id="maleGender"
-                      value="option2"
-                    />
-                    <label class="form-check-label" for="maleGender">Male</label>
-                  </div>
-                  <div class="form-check form-check-inline">
-                    <input
-                      class="form-check-input"
-                      type="radio"
-                      name="inlineRadioOptions"
-                      id="otherGender"
-                      value="option3"
-                    />
-                    <label class="form-check-label" for="otherGender">Other</label>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-md-6 mb-4 pb-2">
-                  <div class="form-outline">
-                    <input type="email" id="emailAddress" class="form-control form-control-lg" placeholder="example@example.com""/>
-                    <label class="form-label" for="phoneNumber">Email Address</label>
-                  </div>
-                </div>
-                <div class="col-md-6 mb-4 pb-2">
-                  <div class="form-outline">
-                    <input type="number" id="phoneNumber" class="form-control form-control-lg" placeholder="07887634567" />
-                    <label class="form-label" for="phoneNumber">Phone Number</label>
-                  </div>
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-12">
-                  <select class="select form-control-lg">
-                    <option value="1" disabled>Choose Class</option>
-                    <option value="2">Class 1</option>
-                    <option value="3">Class 2</option>
-                    <option value="4">Class 3</option>
-                  </select>
-                  <label class="form-label select-label">Choose Class</label>
-                </div>
-              </div>
-              <div class="mt-4 pt-2">
-                <input class="btn btn-primary btn-lg" type="submit" value="Submit" />
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-</section>
-
-
-
+@yield('content')
 
 <div class="foot"><footer class="page-footer font-small stylish-color-dark pt-4">
 <!-- Footer Links -->
@@ -257,7 +156,44 @@
 </div>
 
 
-
-
-</body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
